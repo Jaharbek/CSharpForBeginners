1902e05fd908f00de1b975730493aae954cf0523
Jaharbek <91413239+Jaharbek@users.noreply.github.com>
2023-09-11 12:55:48 +0500

    Добавил примеры использования разных операций

---
+++ MyTestSolution/ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            int[] numbers = new int[10];
+            Random random = new Random();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next();
+                Console.Write(numbers[i] + "\t");
+            }
+            Console.WriteLine();
+        }   
+    }
+}
+     
